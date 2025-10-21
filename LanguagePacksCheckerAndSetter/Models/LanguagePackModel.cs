67ead08691dd22d997fbaf0dea730dddb925b11f
Doriano Benyocki <doriano.benyocki@avl.com>
2025-10-21 15:06:55 +0200

    Feature Update: -Implemented the Install Languages Features what is compatible with .cab files. -Select and Deselect all option is available on the list. While the installation is running is disables the other buttons. - Added a Manifest file and beta versioning - Added an AI generated, free to use icon.ico file - Software usage is guaranteed only with Administrator rights

--- LanguagePacksCheckerAndSetter/Models/LanguagePackModel.cs
+++ LanguagePacksCheckerAndSetter/Models/LanguagePackModel.cs
@@ -1,17 +1,76 @@
-﻿namespace LanguagePacksCheckerAndSetter.Models
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LanguagePacksCheckerAndSetter.Models
 {
-    public class LanguagePackModel
+    public class LanguagePackModel : INotifyPropertyChanged
     {
+        #region Fields
         private string _packageIdentity = string.Empty;
+        private string _filePath = string.Empty;
+        private bool _isSelected = false;
+        private string _languageCode = string.Empty;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        #endregion
+
+        #region Properties
         public string PackageIdentity
         {
             get { return _packageIdentity; }
-            set { _packageIdentity = value; }
+            set
+            {
+                if (_packageIdentity != value)
+                {
+                    _packageIdentity = value;
+                    OnPropertyChanged();
+                }
+            }
         }
+        public string FilePath
+        {
+            get { return _filePath; }
+            set
+            {
+                if (_filePath != value)
+                {
+                    _filePath = value;
+                    OnPropertyChanged();
+                }
 
-        //public override string ToString()
-        //{
-        //    return PackageIdentity;
-        //}
+            }
+        }
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                if (_isSelected != value)
+                {
+                    _isSelected = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string LanguageCode
+        {
+            get { return _languageCode; }
+            set
+            {
+                if (_languageCode != value)
+                {
+                    _languageCode = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        #endregion
+
+        #region Functions
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null!)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        #endregion
+
+
     }
 }
--- LanguagePacksCheckerAndSetter/ViewModels/InstallViewModel.cs
+++ LanguagePacksCheckerAndSetter/ViewModels/InstallViewModel.cs
@@ -1,7 +1,321 @@
-﻿namespace LanguagePacksCheckerAndSetter.ViewModels
+﻿using LanguagePacksCheckerAndSetter.Models;
+using LanguagePacksCheckerAndSetter.Utilities;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace LanguagePacksCheckerAndSetter.ViewModels
 {
-    public class InstallViewModel
+    public class InstallViewModel : INotifyPropertyChanged
     {
-        // TODO - will be implemented
+        #region Fields
+        private string _selectedFolder = string.Empty;
+        private bool _isBusy;
+        private ObservableCollection<LanguagePackModel> _availablePacks = new ObservableCollection<LanguagePackModel>();
+        private ObservableCollection<LogEntry> _operationLog = new ObservableCollection<LogEntry>();
+        private bool _hasSelectedPacks = false;
+        #endregion
+
+        #region Properties
+        public ObservableCollection<LanguagePackModel> AvailablePacks
+        {
+            get { return _availablePacks; }
+            set
+            {
+                if (_availablePacks != value)
+                {
+                    _availablePacks = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public ObservableCollection<LogEntry> OperationLog
+        {
+            get { return _operationLog; }
+            set
+            {
+                if (_operationLog != value)
+                {
+                    _operationLog = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string SelectedFolder
+        {
+            get { return _selectedFolder; }
+            set
+            {
+                if (_selectedFolder != value)
+                {
+                    _selectedFolder = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public bool IsBusy
+        {
+            get { return _isBusy; }
+            set
+            {
+                if (_isBusy != value)
+                {
+                    _isBusy = value;
+                    OnPropertyChanged();
+                    CommandManager.InvalidateRequerySuggested();
+                }
+            }
+        }
+        public bool HasSelectedPacks
+        {
+            get { return _hasSelectedPacks; }
+            set
+            {
+                if (_hasSelectedPacks != value)
+                {
+                    _hasSelectedPacks = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand BrowseFolderCommand { get; }
+        public ICommand InstallSelectedCommand { get; }
+        public ICommand SelectAllCommand { get; }
+        public ICommand DeselectAllCommand { get; }
+        #endregion
+
+        #region Constructors
+        public InstallViewModel()
+        {
+            
+            BrowseFolderCommand = new RelayCommand(async _ => await ExecuteBrowseFolderAsync(), _ => !IsBusy);
+            InstallSelectedCommand = new RelayCommand(async _ => await ExecuteInstallAsync(), _ => !IsBusy && HasSelectedPacks);
+
+            SelectAllCommand = new RelayCommand(_ => SetSelection(true),_ => !IsBusy && AvailablePacks.Count > 0);
+            DeselectAllCommand = new RelayCommand(_ => SetSelection(false),_ => !IsBusy && AvailablePacks.Count > 0);
+            //SelectAllCommand = new RelayCommand(_ => SetSelection(true), _ => AvailablePacks.Count > 0);
+            //DeselectAllCommand = new RelayCommand(_ => SetSelection(false), _ => AvailablePacks.Count > 0);
+
+            AvailablePacks.CollectionChanged += AvailablePacks_CollectionChanged;
+        }
+        #endregion
+
+        #region Functions
+        /// <summary>
+        /// Opens a dialog to allow the user to select a folder and loads CAB files from the selected folder.
+        /// </summary>
+        /// <remarks>This method displays an <see cref="OpenFileDialog"/> configured to select a folder.
+        /// The folder path is determined based on the dummy file name provided in the dialog. If a valid folder is
+        /// selected, the folder path is assigned to the <c>SelectedFolder</c> property, and CAB files from the folder
+        /// are loaded asynchronously.</remarks>
+        /// <returns></returns>
+        private async Task ExecuteBrowseFolderAsync()
+        {
+            var dialog = new OpenFileDialog
+            {
+                CheckFileExists = false,
+                FileName = "Select Folder", // Dummy name
+                Title = "Select a folder containing CAB files"
+            };
+
+            bool? result = dialog.ShowDialog();
+            if (result == true)
+            {
+                string? folderPath = Path.GetDirectoryName(dialog.FileName);
+                if (!string.IsNullOrWhiteSpace(folderPath))
+                {
+                    SelectedFolder = folderPath;
+                    await LoadCabFilesAsync(SelectedFolder);
+                }
+            }
+        }
+
+        private async Task LoadCabFilesAsync(string folderPath)
+        {
+            AvailablePacks.Clear();
+
+            if (!Directory.Exists(folderPath))
+            {
+                OperationLog.Add(new LogEntry
+                {
+                    Message = "Selected folder does not exist.",
+                    Foreground = Brushes.Red
+                });
+                return;
+            }
+
+            string[] cabFiles = Directory.GetFiles(folderPath, "*.cab", SearchOption.TopDirectoryOnly);
+
+            foreach (var file in cabFiles)
+            {
+                string fileName = Path.GetFileName(file);
+                string langCode = ExtractLanguageCode(fileName);
+
+                AvailablePacks.Add(new LanguagePackModel
+                {
+                    PackageIdentity = fileName,
+                    FilePath = file,
+                    LanguageCode = langCode,
+                    IsSelected = false
+                });
+            }
+
+            // Just sort the collection view, no grouping
+            var view = CollectionViewSource.GetDefaultView(AvailablePacks);
+            view.SortDescriptions.Clear();
+            view.SortDescriptions.Add(new SortDescription(nameof(LanguagePackModel.LanguageCode), ListSortDirection.Ascending));
+            view.SortDescriptions.Add(new SortDescription(nameof(LanguagePackModel.PackageIdentity), ListSortDirection.Ascending));
+
+            OperationLog.Add(new LogEntry
+            {
+                Message = $"{cabFiles.Length} CAB files found in {folderPath}.",
+                Foreground = Brushes.SteelBlue
+            });
+
+            await Task.CompletedTask;
+        }
+
+
+        private string ExtractLanguageCode(string fileName)
+        {
+            // Very simple heuristic: look for xx-XX pattern
+            var parts = fileName.Split('-');
+            foreach (var part in parts)
+            {
+                if (part.Length == 5 && part[2] == '-')
+                {
+                    return part;
+                }
+            }
+            return "Unknown";
+        }
+        private async Task ExecuteInstallAsync()
+        {
+            IsBusy = true;
+
+            try
+            {
+                foreach (var pack in AvailablePacks)
+                {
+                    if (!pack.IsSelected) continue;
+
+                    OperationLog.Add(new LogEntry
+                    {
+                        Message = $"Installing {pack.PackageIdentity}...",
+                        Foreground = Brushes.SteelBlue
+                    });
+
+                    var processStartInfo = new ProcessStartInfo
+                    {
+                        FileName = "dism.exe",
+                        Arguments = $"/online /Add-Package /PackagePath:\"{pack.FilePath}\" /Quiet /NoRestart",
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    };
+
+                    using (var process = new Process { StartInfo = processStartInfo })
+                    {
+                        process.Start();
+                        string output = await process.StandardOutput.ReadToEndAsync();
+                        string error = await process.StandardError.ReadToEndAsync();
+                        process.WaitForExit();
+
+                        if (process.ExitCode == 0)
+                        {
+                            OperationLog.Add(new LogEntry
+                            {
+                                Message = $"Successfully installed {pack.PackageIdentity}",
+                                Foreground = Brushes.Green
+                            });
+                        }
+                        else
+                        {
+                            OperationLog.Add(new LogEntry
+                            {
+                                Message = $"Failed to install {pack.PackageIdentity} (ExitCode {process.ExitCode}). " +
+                                          $"Output: {output} Error: {error}",
+                                Foreground = Brushes.Red
+                            });
+                        }
+                    }
+                }
+
+                OperationLog.Add(new LogEntry
+                {
+                    Message = "Installation process completed.",
+                    Foreground = Brushes.Green
+                });
+            }
+            catch (Exception ex)
+            {
+                OperationLog.Add(new LogEntry
+                {
+                    Message = $"Error occurred during installation: {ex.Message}",
+                    Foreground = Brushes.Red
+                });
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+        private void SetSelection(bool isSelected)
+        {
+            foreach (var pack in AvailablePacks)
+            {
+                pack.IsSelected = isSelected;
+            }
+            HasSelectedPacks = isSelected;
+        }
+
+        private void AvailablePacks_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if (e.NewItems != null)
+            {
+                foreach (LanguagePackModel pack in e.NewItems)
+                {
+                    pack.PropertyChanged += Pack_PropertyChanged;
+                }
+            }
+            if (e.OldItems != null)
+            {
+                foreach (LanguagePackModel pack in e.OldItems)
+                {
+                    pack.PropertyChanged -= Pack_PropertyChanged;
+                }
+            }
+
+            UpdateHasSelectedPacks();
+        }
+
+        private void Pack_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(LanguagePackModel.IsSelected))
+            {
+                UpdateHasSelectedPacks();
+            }
+        }
+
+        private void UpdateHasSelectedPacks()
+        {
+            HasSelectedPacks = AvailablePacks.Any(p => p.IsSelected);
+            CommandManager.InvalidateRequerySuggested();
+        }
+        #endregion
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null!)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        #endregion
     }
 }
